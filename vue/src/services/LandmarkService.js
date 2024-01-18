b173830a1f20ee683886f668438f0597cc30e9e9
Daniel Hunt <dfhunt24@gmail.com>
2024-01-18 15:03:24 -0500

    Changed the POST in LandmarkService to a GET

--- vue/src/services/LandmarkService.js
+++ vue/src/services/LandmarkService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default {
 
     returnLandmarks(city) {
-      return axios.post('/landmarks/' + city)
+      return axios.get('/landmarks/' + city)
     }
   
   }
