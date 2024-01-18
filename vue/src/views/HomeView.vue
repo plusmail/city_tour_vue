bf85808114eeb65e7957d30b5edf4a857d059cbd
Charles Thompson <contact@charles.social>
2024-01-18 09:58:22 -0500

    City dropdown

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <SearchHeader @citySelected="citySelection" />
+<Dropdown @citySelected="citySelection" v-model="selectedCity" :options="groupedCities" optionLabel="label" optionGroupLabel="label" optionGroupChildren="items" placeholder="Select a City" class="w-full md:w-14rem">
+    <template #optiongroup="slotProps">
+        <div class="flex align-items-center">
+            <img :alt="slotProps.option.label" src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png" :class="`mr-2 flag flag-${slotProps.option.code.toLowerCase()}`" style="width: 18px" />
+            <div>{{ slotProps.option.label }}</div>
+        </div>
+    </template>
+</Dropdown>
     <DeckGLMap/>
     <GoogleMap/>
     
