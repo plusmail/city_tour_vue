629017eb210ebc85ab9c1a4ec8d9fe7700250c1c
Daniel Hunt <dfhunt24@gmail.com>
2024-01-23 13:42:22 -0500

    Added EventHeader with calendar

--- vue/src/components/EventHeader.vue
+++ vue/src/components/EventHeader.vue
@@ -1,29 +1,33 @@
 <template>
-    <!-- Date and Time Dropdown -->
-    <Calendar id="calendar-12h" v-model="selectedDateTime" showTime hourFormat="24" :minDate="currentDate" dateFormat="yyyy-mm-dd"/>
+  <!-- Date and Time Dropdown -->
+  <Calendar
+    id="calendar-12h"
+    v-model="selectedDateTime"
+    showTime
+    hourFormat="24"
+    :minDate="currentDate"
+    dateFormat="yyyy-mm-dd"
+  />
 </template>
-  
-  
-    
-  <script>
-  import { ref, watch } from 'vue';
-  import Calendar from 'primevue/calendar';
-  
-  
-  export default {
-    components: {
-        Calendar
-    },
-    setup(props, { emit }) {
-      const selectedDateTime = ref();
-    
-      watch(selectedDateTime, (newValue) => {
-        emit('update:modelValue', newValue);
-      });
-  
-      return {
-        selectedDateTime
-      };
-    }
-  };
-  </script>
+
+<script>
+import { ref, watch } from "vue";
+import Calendar from "primevue/calendar";
+
+export default {
+  components: {
+    Calendar
+  },
+  setup(props, { emit }) {
+    const selectedDateTime = ref();
+
+    watch(selectedDateTime, (newValue) => {
+      emit("update:modelValue", newValue);
+    });
+
+    return {
+      selectedDateTime
+    };
+  },
+};
+</script>
